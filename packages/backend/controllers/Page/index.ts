48e01078b4f0ad5d5b327e41a4bb4229104f342e
Asher Lien <birdym.tw@gmail.com>
2026-03-09 14:49:55 +0800

    更新

--- packages/backend/controllers/Page/index.ts
+++ packages/backend/controllers/Page/index.ts
@@ -1,13 +1,9 @@
 import { CreatePageController } from './CreatePageController'
 import { GetAllPagesController } from './GetAllPagesController'
-import { GetPageController } from './GetPageController'
 import { UpdatePageController } from './UpdatePageController'
-import { DeletePageController } from './DeletePageController'
 
 export const PageController = {
   create: CreatePageController,
   getAll: GetAllPagesController,
-  getOne: GetPageController,
   update: UpdatePageController,
-  delete: DeletePageController,
 }
--- packages/backend/routes/PageRoutes.ts
+++ packages/backend/routes/PageRoutes.ts
@@ -5,8 +5,6 @@ const router = Router()
 
 router.post('/', authenticateUser, PageController.create)
 router.get('/', authenticateUser, PageController.getAll)
-router.get('/:id', authenticateUser, PageController.getOne)
 router.patch('/:id', authenticateUser, PageController.update)
-router.delete('/:id', authenticateUser, PageController.delete)
 
 export default router
