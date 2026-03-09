460c1a8b8bd808e825527e68d6f46d1a03639e63
Asher Lien <birdym.tw@gmail.com>
2026-03-09 17:48:27 +0800

    e鞥心

--- packages/frontend/app/stores/useMenuStore.ts
+++ packages/frontend/app/stores/useMenuStore.ts
@@ -2,9 +2,9 @@ import { WebAuthRoutes } from '@monorepo/libs/enum'
 
 export const useMenuStore = defineStore('menu', () => {
   const menuItems = ref([
+    { label: '個人資料', icon: 'i-lucide-layout-dashboard', to: WebAuthRoutes.ADMIN_INDEX },
     { label: '聊天機器人', icon: 'i-lucide-bot', to: WebAuthRoutes.ADMIN_CHATBOT },
-    { label: '一頁網站', icon: 'i-lucide-layout-template', to: WebAuthRoutes.ADMIN_WEBSITE },
-    { label: '儀表板', icon: 'i-lucide-layout-dashboard', to: WebAuthRoutes.ADMIN_INDEX }
+    { label: '一頁網站', icon: 'i-lucide-layout-template', to: WebAuthRoutes.ADMIN_WEBSITE }
   ])
 
   return {
