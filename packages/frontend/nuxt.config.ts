931a46f938bebd39c1553a61190310756c4a6cd8
Asher Lien <birdym.tw@gmail.com>
2026-03-17 14:21:37 +0800

    done

--- packages/frontend/nuxt.config.ts
+++ packages/frontend/nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/ui', '@pinia/nuxt'],
+
+  components: {
+    dirs: []
+  },
   devtools: {
     enabled: true
   },
