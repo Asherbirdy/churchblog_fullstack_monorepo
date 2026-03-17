22b01c089f79d722561d65600aa18198458bfe3f
Asher Lien <birdym.tw@gmail.com>
2026-03-17 16:55:14 +0800

    更新

--- packages/backend/controllers/Auth/index.ts
+++ packages/backend/controllers/Auth/index.ts
@@ -1,11 +1,11 @@
-import { RegisterController } from './RegisterController'
+import { AdminInitController } from './AdminInitController'
 import { LoginController } from './LoginController'
 import { LogoutController } from './LogoutController'
 import { RefreshTokenController } from './RefreshTokenController'
 import { CheckValidTokenController } from './CheckValidTokenController'
 
 export const AuthController = {
-  register: RegisterController,
+  adminInit: AdminInitController,
   login: LoginController,
   logout: LogoutController,
   refreshToken: RefreshTokenController,
