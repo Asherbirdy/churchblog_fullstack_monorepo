859b5e5bf676ba8cf2866cea4fcb3e45eab79ce3
Asher Lien <birdym.tw@gmail.com>
2026-03-17 14:23:56 +0800

    asd

--- packages/frontend/app/components/TiptapEditor.vue
+++ packages/frontend/app/components/TiptapEditor.vue
@@ -29,7 +29,7 @@ const editor = useEditor({
 
 watch(model, (val) => {
   if (editor.value && editor.value.getHTML() !== val) {
-    editor.value.commands.setContent(val, false)
+    editor.value.commands.setContent(val)
   }
 })
 </script>
