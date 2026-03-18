f697f56270b4635e808cd02ac54724e840affe3e
Asher Lien <birdym.tw@gmail.com>
2026-03-18 15:31:29 +0800

    adl

---
+++ packages/backend/utils/checkPersmission.ts
@@ -0,0 +1,24 @@
+
+interface checkPersmissionPayload {
+  userId: string
+  role?: string
+}
+
+export const checkPersmission = (requestUser: { role: string; userId: string }, resourceUserId: checkPersmissionPayload) => {
+  // console.log(requestUser)
+  // console.log(resourceUserId)
+  // console.log(typeof resourceUserId)
+  // console.log(requestUser.userId === resourceUserId.userId)
+  // if (requestUser.role === 'dev') return
+  if (requestUser.userId === resourceUserId.userId) return
+
+  throw new Error(
+    'Not authorized to access this route [checkPersmission]'
+  )
+}
+
+export const getUserIdByString = (obj: { userId: string }) => {
+  return {
+    userId: obj.userId.toString(),
+  }
+}
--- packages/backend/utils/index.ts
+++ packages/backend/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './createTokenUser'
 export * from './jwt'
 export * from './emailService'
+export * from './checkPersmission'
