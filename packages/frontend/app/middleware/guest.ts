8678b4e53ace5d5aea2cf40bf1c5105c8760aa81
Asher Lien <birdym.tw@gmail.com>
2026-03-20 13:56:26 +0800

    更新

--- packages/frontend/app/middleware/guest.ts
+++ packages/frontend/app/middleware/guest.ts
@@ -1,10 +1,19 @@
 import { useUserApi } from '~/api'
-import { PrivateRoutes } from '~/enum'
+import { CookieEnums, PrivateRoutes, PublicRoutes } from '~/enum'
 
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtRouteMiddleware(async (to) => {
+  const refreshToken = useCookie(CookieEnums.RefreshToken).value
+
+  if (!refreshToken) {
+    if (to.path === PublicRoutes.LOGIN) return
+    return navigateTo(PublicRoutes.LOGIN)
+  }
+
   const { error } = await useUserApi.showMe()
 
+  // 沒有ERROR 表示已經登入
   if (!error.value) {
     return navigateTo(PrivateRoutes.ADMIN_HOME)
   }
+  return navigateTo(PrivateRoutes.ADMIN_HOME)
 })
