37014027f6bed71d73379953f35a9cc44e601ca2
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2026-03-20 11:15:42 +0800

    fix/page權限邏輯 (#32)

--- packages/backend/routes/PageRoutes.ts
+++ packages/backend/routes/PageRoutes.ts
@@ -7,7 +7,7 @@ const router = Router()
 
 router.post('/', authenticateUser, authAccess(AccessEnum.PAGE), PageController.create)
 router.get('/', authenticateUser, PageController.getAll)
-router.get('/info/:id', authenticateUser, authAccess(AccessEnum.PAGE), PageController.getOne)
+router.get('/info/:id', authenticateUser, PageController.getOne)
 router.get('/online', PageController.getOnline)
 router.get('/route/:routeName', PageController.getByRouteName)
 router.patch('/update/:id', authenticateUser, authAccess(AccessEnum.PAGE), PageController.update)
