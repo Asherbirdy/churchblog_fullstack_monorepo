167c32ee32c10c20aa932d0e99fea08cb6900f59
Asher Lien <birdym.tw@gmail.com>
2026-03-23 14:53:01 +0800

    更新

---
+++ packages/backend/prisma/migrations/20260323065249_add_chatkey_edit/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `online` on the `ChatTopic` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ChatTopic" DROP COLUMN "online";
