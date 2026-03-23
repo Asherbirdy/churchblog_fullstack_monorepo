019abddbb5b12dfbdf5e5ffc334865179743791b
Asher Lien <birdym.tw@gmail.com>
2026-03-23 15:01:33 +0800

    更新

--- packages/backend/controllers/ChatTopic/CreateChatTopicController.ts
+++ packages/backend/controllers/ChatTopic/CreateChatTopicController.ts
@@ -7,9 +7,6 @@ export const CreateChatTopicController = async (req: Request, res: Response) => 
   const { name, keywords } = req.body
   if (!name) throw new BadRequestError('NAME_REQUIRED')
 
-  const existing = await prisma.chatTopic.findFirst({ where: { name } })
-  if (existing) throw new BadRequestError('CHAT_TOPIC_NAME_ALREADY_EXISTS')
-
   const chatTopic = await prisma.chatTopic.create({
     data: {
       name,
