f26d9e09ad7bc2494f333fe8de56f62735d7a487
Asher Lien <birdym.tw@gmail.com>
2026-03-24 09:26:17 +0800

    更新

--- packages/chatbot/src/api/http/index.ts
+++ packages/chatbot/src/api/http/index.ts
@@ -3,10 +3,9 @@ import _RequstInterceptors from './axios/requestInterceptors'
 import { url } from './config'
 
 
-// ** Clicugo API（使用 dynamicBaseURL 動態獲取 baseURL，優先使用 ConfigStore 的 meta.domain）
 const useApiRequest = new Axios({
   directlyGetData: true,
-  baseURL: url, // * dynamicBaseURL 為跑函示的 baseUrl
+  baseURL: url,
   timeout: 1000 * 60 * 5,
   interceptors: _RequstInterceptors,
   abortRepetitiveRequest: true,
