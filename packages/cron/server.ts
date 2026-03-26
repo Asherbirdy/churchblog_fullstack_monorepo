14108769c789d37d427cd69af661fad02666ecdd
Asher Lien <birdym.tw@gmail.com>
2026-03-26 17:34:25 +0800

    更新

--- packages/cron/server.ts
+++ packages/cron/server.ts
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
 import http from 'http'
 import cron from 'node-cron'
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/' || req.url === '/index.html') {
